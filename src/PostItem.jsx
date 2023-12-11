72c7d46dc53aa3b9bf0d68c484d03b800f9898be
Isaac Rakstad <iscrakdev@gmail.com>
2023-12-11 11:44:24 -0600

    added post-item.jsx component

---
+++ src/PostItem.jsx
@@ -0,0 +1,23 @@
+const PostItem = () => {
+  return (
+    <div className="post-item">
+      <div className="row-1">
+        <h2>Post Title</h2>
+        <p onClick={() => console.log("delete")}>🗑</p>
+      </div>
+      <div className="row-2">
+        <p>Author Name</p>
+      </div>
+      <div className="row-3">
+        <p>limited post text</p>
+      </div>
+      <div className="row-4">
+        <p onClick={() => console.log("read more")}>Read More...</p>
+        <p>Upvotes: upvotes</p>
+        <p>Downvotes: downvotes</p>
+      </div>
+    </div>
+  );
+};
+
+export default PostItem
