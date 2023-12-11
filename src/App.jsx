f7ad45a4e2613cef6dcd64506013c7aa12f25160
Isaac Rakstad <iscrakdev@gmail.com>
2023-12-11 11:50:51 -0600

    added postlist to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import PostList from "./PostList";
+
 
 function App() {
+  const posts = [1, 2, 3, 4]
+
   return (
     <div className="App">
-      
+      <PostList posts={posts}></PostList>
     </div>
   );
 }
--- src/PostList.jsx
+++ src/PostList.jsx
@@ -3,9 +3,11 @@ import PostItem from "./PostItem";
 const PostList = ({ posts }) => {
   return (
     <div className="post-list">
-      {posts.map((post) => (
-        <PostItem post={post}></PostItem>
-      ))}
+        {posts.map((post) => (
+          <PostItem post={post}></PostItem>
+        ))}
     </div>
   );
 };
+
+export default PostList;
