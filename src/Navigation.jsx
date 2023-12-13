65049cb74bf4deffe7a8b894cf9c83ff683dd799
Isaac Rakstad <iscrakdev@gmail.com>
2023-12-13 10:35:30 -0600

    removed unneccesary code

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,9 +1,6 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
 
 const Navigation = () => {
-  const [author, setAuthor] = useState("");
-
   return (
     <div>
       <h1>React Bleg Project</h1>
